1bbb57a70e21aee28d2d2f8c0c8915143b0d517d
gabreb <62906858+gabreb@users.noreply.github.com>
2020-07-14 13:33:28 -0700

    hi

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,17 +3,17 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
-	
+	T val;
 	public ArrayList() {
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
 		
-		return null;
+		return null; //?????
 	}
 	
 	public void add(T val) {
-		
+		this.val = val;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
